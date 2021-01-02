a2b16c2bb9fefc371e6e4e7011a734a5d55001cd
fgasdia <fgasdia@noreply.github.com>
2021-01-02 12:12:03 -0700

    build in file-writing

--- CSV/footerskip.jl
+++ CSV/footerskip.jl
@@ -1,4 +1,18 @@
+using Printf
 using CSV
 
-file = "footerskip.csv"
-raw = CSV.File(file; skipto=3, footerskip=3)
+open("foo_rn.csv", "w") do io
+    @printf(io, "1, a\r\n")
+    @printf(io, "2, b\r\n")
+    @printf(io, "3, c\r\n")
+end
+
+open("foo_n.csv", "w") do io
+    @printf(io, "1, a\n")
+    @printf(io, "2, b\n")
+    @printf(io, "3, c\n")
+end
+
+CSV.File("foo_rn.csv"; skipto=1, footerskip=2)
+
+CSV.File("foo_n.csv"; skipto=1, footerskip=2)
