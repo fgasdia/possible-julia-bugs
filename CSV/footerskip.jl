5f61ac35de15417d625626b84e1a8781bb076cd7
fgasdia <fgasdia@noreply.github.com>
2021-01-02 13:05:16 -0700

    change text example

--- CSV/footerskip.jl
+++ CSV/footerskip.jl
@@ -6,6 +6,22 @@ using CSV
 
 # Let's say I want to read rows 1, 2, 3
 
+# MWE of original problem
+open("bar_n.csv", "w") do io
+    @printf(io, "1, a\n")
+    @printf(io, "2, b\n")
+    @printf(io, "3, c\n")
+    @printf(io, "ignore this line\n")
+    @printf(io, "\n")
+    @printf(io, "and this line\n")
+end
+
+# BROKEN: In linux, this reads rows 1, 2, 3, and "ignore this line"
+CSV.File("bar_n.csv"; skipto=1, footerskip=2)
+
+
+
+
 # Lines end in "\n" only
 open("foo_n.csv", "w") do io
     @printf(io, "1, a\n")
@@ -15,25 +31,11 @@ open("foo_n.csv", "w") do io
     @printf(io, "\n")
 end
 
-# on Windows, this reads rows 1, 2, 3???
-# on Linux, this reads rows 1, 2, 3, 4
+# BROKEN: on Linux, this reads rows 1, 2, 3, 4
 CSV.File("foo_n.csv"; skipto=1, footerskip=1)
 
-# This doesn't happen without the extra blank line at the end.
-# In practice, this problem occurs if there is a text line that we also want skipped
-# at the end. Here's an example:
-open("bar_n.csv", "w") do io
-    @printf(io, "1, a\n")
-    @printf(io, "2, b\n")
-    @printf(io, "3, c\n")
-    @printf(io, "\n")
-    @printf(io, "ignore this line\n")
-end
 
-# In linux, this only reads rows 1, 2, 3
-CSV.File("bar_n.csv"; skipto=1, footerskip=1)
-
-# Lines end in "\r\n"
+# No difference if lines end in "\r\n"
 open("foo_rn.csv", "w") do io
     @printf(io, "1, a\r\n")
     @printf(io, "2, b\r\n")
@@ -42,5 +44,29 @@ open("foo_rn.csv", "w") do io
     @printf(io, "\r\n")
 end
 
-# on Windows and Linux, this reads rows 1, 2, 3, 4
+# BROKEN: on Windows and Linux, this reads rows 1, 2, 3, 4
 CSV.File("foo_rn.csv"; skipto=1, footerskip=1)
+
+
+# It works without the extra empty line at the end
+open("foo2_n.csv", "w") do io
+    @printf(io, "1, a\n")
+    @printf(io, "2, b\n")
+    @printf(io, "3, c\n")
+    @printf(io, "4, d\n")
+end
+
+# WORKING: on Linux, this reads rows 1, 2, 3
+CSV.File("foo2_n.csv"; skipto=1, footerskip=1)
+
+
+# No difference if lines end in "\r\n"
+open("foo2_rn.csv", "w") do io
+    @printf(io, "1, a\r\n")
+    @printf(io, "2, b\r\n")
+    @printf(io, "3, c\r\n")
+    @printf(io, "4, d\r\n")
+end
+
+# WORKING: on Linux, this reads rows 1, 2, 3
+CSV.File("foo2_rn.csv"; skipto=1, footerskip=1)
