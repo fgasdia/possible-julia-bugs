33af62ae9776684e21f259a6ea87f540ef672ed2
fgasdia <fgasdia@noreply.github.com>
2021-01-02 12:14:41 -0700

    Add an extra blank line

--- CSV/footerskip.jl
+++ CSV/footerskip.jl
@@ -5,12 +5,14 @@ open("foo_rn.csv", "w") do io
     @printf(io, "1, a\r\n")
     @printf(io, "2, b\r\n")
     @printf(io, "3, c\r\n")
+    @printf(io, "\r\n")
 end
 
 open("foo_n.csv", "w") do io
     @printf(io, "1, a\n")
     @printf(io, "2, b\n")
     @printf(io, "3, c\n")
+    @printf(io, "\n")
 end
 
 CSV.File("foo_rn.csv"; skipto=1, footerskip=2)
